b051ccb3c74751abd35f2ff77afa4035b3a306ec
jenna-anders <32047357+jenna-anders@users.noreply.github.com>
2025-11-18 21:53:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -471,4 +471,6 @@ def on_leave(data):
     leave_room(code)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
